6489f1cd7ce3a3af3cbc5465eef015e51e74b5c5
Albert <gooferstotjyu@yandex.ru>
2022-08-14 20:07:54 +0600

    -update 14/08/2022

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+//import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('shaun');
+
+  const clickHandler = () => {
+    setName('chun-li');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My name is {name}</Text>
+      <View style={styles.ButtonContainer}>
+        <Button title='Update State' onPress={clickHandler} />
+      </View>
     </View>
   );
 }

fa44ea2d4ac860c412c6ca4366c59f3571a240bd
Albert <gooferstotjyu@yandex.ru>
2022-08-17 20:26:30 +0600

    update

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
 //import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('shaun');
-  const [age, setAge] = useState(30);
+  const [people, setPeople] = useState([
+    { name: 'shaun', key: '1' },
+    { name: 'yoshi', key: '2' },
+    { name: 'mario', key: '3' },
+    { name: 'luigi', key: '4' },
+    { name: 'peach', key: '5' },
+    { name: 'toad', key: '6' },
+    { name: 'bowser', key: '7' },
+  ]);
+
 
   return (
+
     <View style={styles.container}>
-      <Text>Enter name:</Text>
-       <TextInput style={styles.input} placeholder='К примеру Митрофан' onChangeText={(value) => setName(value)}/>
-       {/*placeholder - тескт который виден до ввода*/}
-
-      <Text>Enter age:</Text>
-       <TextInput style={styles.input} placeholder='к примеру 20' onChangeText={(value) => setAge(value)} keyboardType='numeric'/>
-      <Text>Name: {name}, Age: {age}</Text>
+ 
+      <ScrollView>
+        {people.map((item) => (
+          <View key={item.key}>
+            <Text style={styles.item}>{item.name}</Text>
+          </View>
+        ))}
+      </ScrollView >
     </View>
-
-    
   );
 }
 
@@ -28,11 +36,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  input: {
-    borderWidth: 1,     //толщина рамка
-    borderColor: '#777',   //цвет рамки    
-    padding: 10,     //отступ рамки от текста который внутри 
-    margin: 10,     //отступ  от рамки (если есть другие элементы они сдвинутся)
-    width: 200,     //ширина рамки
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24
   }
 });

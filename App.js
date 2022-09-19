c80c9b75ce9fa2170ea7d1a3e0c977b4bf64982e
Albert <gooferstotjyu@yandex.ru>
2022-09-20 01:15:39 +0600

    -update 20.09.2022

--- App.js
+++ App.js
@@ -10,9 +10,13 @@ const Start = () => {
       <View style={styles.main}>
         <Text style={styles.mainText}>Дневник заданий</Text>
       </View>
+      <View>
+        <Text>Бицепс + Трицепс</Text>
+        <Text>1. 10 кг(12) - 15 кг - 20 кг - 25 кг (6)      Штанга</Text>
+      </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>Все права не защищены</Text>
-      </View> 
+      </View>
     </View>
   );
 }
@@ -22,22 +26,23 @@ const styles = StyleSheet.create({
     flex: 2,
     backgroundColor: '#00FF00'
   },
-  headerText: { 
-    fontSize: 30, 
+  headerText: {
+    fontSize: 30,
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
     paddingTop: 20
   },
   main: {
-    flex: 15, 
+    flex: 15,
     backgroundColor: 'white'
   },
   mainText: {
-    fontSize: 20
+    fontSize: 20,
+    textAlign: 'center'
   },
   footer: {
-    flex: 1, 
+    flex: 1,
     backgroundColor: 'steelblue'
   },
   footerText: {

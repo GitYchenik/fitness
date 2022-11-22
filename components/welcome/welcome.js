62ce484b0124653732f51208daaf4428a0df115a
Albert <gooferstotjyu@yandex.ru>
2022-11-23 01:53:29 +0600

    -update 23.11.2022

---
+++ components/welcome/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+
+const Welcome = () => {
+    return (
+        <View style={
+            { flex: 1, backgroundColor: '#00FF00' }}>
+            <Text>Приветики</Text>
+        </View>
+    )
+}
+
+export default Welcome;

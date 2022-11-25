e5701c33fd7eea4be1689b64b2b11e4656d5392c
Albert <gooferstotjyu@yandex.ru>
2022-11-25 15:58:18 +0600

    -update добавления изображения

--- components/test/HomeScreen.js
+++ components/test/HomeScreen.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
-export default function HomeScreen( {navigation} ) {
+export default function HomeScreen({ navigation }) {
     const LoadScene = () => {
         setTimeout(
-            ()=>{ 
-            navigation.navigate('ProfileScreen')
-        }, 3000);
+            () => {
+                navigation.navigate('ProfileScreen')
+            }, 5000);
     }
-        //navigation.navigate('ProfileScreen')
-    
+    //navigation.navigate('ProfileScreen')
+
     return (
-        <View>
-            <Text>Страница номер 1</Text> 
-            <LoadScene />
-        </View>
+        <>
+            <View>{<LoadScene />}</View>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Image source={{uri: 'https://snack-web-player.s3.us-west-1.amazonaws.com/v2/46/static/media/react-native-logo.79778b9e.png'}} />
+                <Text>Жить здорово</Text>
+            </View>
+        </>
     );
-} 
+}
+
+/*
+            <View style = {{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text>Жить здорово</Text>
+                <Image source={require('../media/welcome.jpg')} />
+
+            </View>
+*/
--- components/test/ProfileScreen.js
+++ components/test/ProfileScreen.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { View, Text, Alert } from 'react-native';
-
-const tims = () => {
-    setTimeout(function() {
-        Alert.alert('Ghbdtdbnnb')      
-    }, 5000);
-}
+import { View, Text, Image } from 'react-native';
 
 export default function ProfileScreen() {
     return (
         <View>
             <Text>Страница номер 2</Text>
+            <Image source={require('../media/welcome.jpg')} />
         </View>
     );
 }

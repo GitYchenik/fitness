4af8fd66997f2f7f9fb21cf4b7109516a46d6f0f
Albert <gooferstotjyu@yandex.ru>
2022-12-01 03:21:46 +0600

    -update создания компонента для хранения данных

---
+++ components/Data/Data.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+
+const Data_db = [
+    { id: 1, znachenie: 1},
+    { id: 1, znachenie: 1},
+    { id: 1, znachenie: 1},
+    { id: 1, znachenie: 1}
+]
+
+export default Data_db;

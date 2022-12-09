62242b0b9e756b91b4e6ee8e2d1aef740e2137e4
Albert <gooferstotjyu@yandex.ru>
2022-12-10 01:38:01 +0600

    ++++

--- components/start/Navigator/ProfileScreen/Touchable_Main.js
+++ components/start/Navigator/ProfileScreen/Touchable_Main.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View, Text, TouchableHighlight, StyleSheet} from 'react-native';
+import { View, Text, TouchableHighlight, StyleSheet } from 'react-native';
 
-const Touchable_Main = ({ navigation }) => {
 
+const Touchable_Main = (props) => {
+  const { navigation, data_id, data_zn } = props;
+  console.log(data_zn)
   return (
     <View style={styles.mainblock}>
-      <Text style={styles.zagolovok}> Hello  !</Text>
-      <TouchableHighlight onPress={() => navigation.navigate("Main", { data: { title: 'Hello World'}})} style={styles.button} underlayColor="blue">
-        <Text> dfsdfsfsfsdfsdf</Text>
+      <Text style={styles.zagolovok}>День{data_id}! </Text>
+      <TouchableHighlight onPress={() => navigation.navigate("Main")} style={styles.button} underlayColor="blue">
+        <View>{data_zn.map((item) => {
+          return (
+            <View>
+              <Text>{item.znachenie}</Text>
+            </View>
+          )
+        })}
+        </View>
       </TouchableHighlight>
     </View>
   )
